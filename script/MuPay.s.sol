d1b9891d42099bb113d56f96b1e0559c19b258e4
Aashish Paliwal <aashish.paliwal@live.com>
2025-02-23 08:46:51 +0530

    Add: script

---
+++ script/MuPay.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import {Script} from "forge-std/Script.sol";
+import {MuPay} from "../src/MuPay.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns (MuPay) {
+        vm.startBroadcast();
+
+        MuPay muPay = new MuPay();
+
+        vm.stopBroadcast();
+        return muPay;
+    }
+}
