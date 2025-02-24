09db9f0d9a91d54dba81012b579c8c253a786cbd
Aashish Paliwal <aashish.paliwal@live.com>
2025-02-24 12:47:08 +0530

    add: test scenario for successful redeem channel

--- test/MuPay.t.sol
+++ test/MuPay.t.sol
@@ -292,4 +292,73 @@ contract MuPayTest is Test {
             "Test Case 2: Incorrect payer withdraw after blocks stored"
         );
     }
+
+    // Redeem Channel Test cases
+
+    function testRedeemChannelSuccess() public {
+        // Setup parameters
+        bytes32 trustAnchor = 0x7cacb8c6cc65163d30a6c8ce47c0d284490d228d1d1aa7e9ae3f149f77b32b5d;
+        bytes32 finalToken = 0x484f839e58e0b400163856f9b4d2c6254e142d89d8b03f1e33a6717620170f30;
+        uint256 amount = 1e18;
+        uint256 numberOfTokens = 100;
+        uint256 merchantWithdrawAfterBlocks = 10;
+        uint256 payerWithdrawAfterBlocks = 100;
+        uint256 numberOfTokensUsed = 50;
+
+        // Expect event emission
+        vm.expectEmit(true, true, false, true);
+        emit MuPay.ChannelCreated(
+            payer,
+            merchant,
+            amount,
+            numberOfTokens,
+            merchantWithdrawAfterBlocks
+        );
+
+        // Execute the function call
+        vm.prank(payer);
+        muPay.createChannel{value: amount}(
+            merchant,
+            trustAnchor,
+            amount,
+            numberOfTokens,
+            merchantWithdrawAfterBlocks,
+            payerWithdrawAfterBlocks
+        );
+
+        // Move forward by 1 block
+        vm.roll(block.number + 10);
+
+        (, uint256 storedAmount, uint256 storedNumberOfToken, , ) = muPay
+            .channelsMapping(payer, merchant);
+
+        uint256 payableAmountMerchant = (storedAmount * numberOfTokensUsed) /
+            storedNumberOfToken;
+        uint256 payableAmountPayer = storedAmount - payableAmountMerchant;
+
+        vm.expectEmit(true, true, false, true);
+        emit MuPay.ChannelRedeemed(
+            payer,
+            merchant,
+            payableAmountMerchant,
+            finalToken,
+            numberOfTokensUsed
+        );
+
+        vm.expectEmit(true, true, false, true);
+        emit MuPay.ChannelRefunded(payer, merchant, payableAmountPayer);
+
+        vm.prank(merchant);
+        muPay.redeemChannel(payer, finalToken, numberOfTokensUsed);
+
+        (, uint256 retrievedAmount, , , ) = muPay.channelsMapping(
+            payer,
+            merchant
+        );
+        assertEq(
+            retrievedAmount,
+            0,
+            "Channel should be deleted after redeeming"
+        );
+    }
 }
